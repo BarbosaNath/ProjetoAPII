adafacb4df38643ab2e5579db930c2e434dbb082
brenolima54 <breno54lima54@gmail.com>
2022-11-13 18:09:31 -0300

    Arquivo de funções de manuseio dos produtos

---
+++ products.py
@@ -0,0 +1,13 @@
+import database.database as db
+    
+def create_product(database_file, product_name, data):
+    db.create_table(database_file, product_name, data)
+
+def remove_product(database_file, product_name):
+    db.remove_table(database_file, product_name)
+
+def list_product(database_file, product_name):
+    return db.get_table_as_dict(database_file, product_name, "id")
+
+def edit_product(database_file, product_name, what, to, where):
+    db.edit_element(database_file, product_name, what, to, where)
