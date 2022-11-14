934097a718cfc35c536dd37130bdbb6e2d8e53cc
Nath <playerxmatt@gmail.com>
2022-11-14 11:33:37 -0300

    Renomeada a função list_products para get_products

--- products.py
+++ products.py
@@ -6,7 +6,7 @@ def create_product(database_file, product_name, data):
 def remove_product(database_file, product_name):
     db.remove_table(database_file, product_name)
 
-def list_product(database_file, product_name):
+def get_product(database_file, product_name):
     return db.get_table_as_dict(database_file, product_name, "id")
 
 def edit_product(database_file, product_name, what, to, where):
