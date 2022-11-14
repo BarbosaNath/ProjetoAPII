eaa6ce1eff6bbe2f1196ab2f279a62a141e4f9f8
Nath <playerxmatt@gmail.com>
2022-11-14 11:53:10 -0300

    Alterada a sintax da função create_tag_group(database_file, data) -> create_tag_group(database_file)

--- tags.py
+++ tags.py
@@ -5,8 +5,8 @@ def get_all_groups():
     return db.get_all_tables("database/tag_groups.db")
 
 
-def create_tag_group(tag_group_name, data):
-    db.create_table("database/tag_groups.db", tag_group_name, data)
+def create_tag_group(tag_group_name):
+    db.create_table("database/tag_groups.db", tag_group_name, ["tag TEXT NOT NULL PRIMARY KEY"])
 
 
 def remove_tag_group(tag_group_name):
@@ -27,15 +27,10 @@ def add_tags_to_group(tag_group_name, tag):
     
 
 if __name__ == "__main__":
-    create_tag_group("tamanho", ["tag TEXT NOT NULL PRIMARY KEY"])
-    create_tag_group("cor"    , ["tag TEXT NOT NULL PRIMARY KEY"])
-
-    add_tags_to_group("cor",   "branco")
-    add_tags_to_group("cor",    "preto")
-    add_tags_to_group("cor", "vermelho")
-    add_tags_to_group("cor",     "azul")
+    create_tag_group("tamanho")
+    create_tag_group("cor")
 
     print(get_all_groups())
     
-    print(get_tag_group("tamanho"))
-    print(get_tag_group("cor"))
+    print(repr(get_tag_group("tamanho")))
+    print(repr(get_tag_group("cor")))
