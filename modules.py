f0b67d1d8e612c2d5c36715070d79dbaf5f8de03
Cláudio Jr <claudio.junior98@aluno.ifce.edu.br>
2022-12-02 20:46:55 -0300

    Adicionado funções de manuseio dos modulos

--- modules.py
+++ modules.py
@@ -1,13 +1,24 @@
 import database.database as db
-    
-def create_module(database_file, module_name, data):
-    db.create_table(database_file, module_name, data)
 
-def remove_module(database_file, module_name):
-    db.remove_table(database_file, module_name)
+def create_module(module_name):
+    db.create_table('database/modules.db', module_name, ['code TEXT NOT NULL PRIMARY KEY','tag_groups TEXT'])
 
-def get_module(database_file, module_name):
-    return db.get_table_as_dict(database_file, module_name, "id")
+def get_all_modules():
+    return db.get_all_tables("database/modules.db")
 
-def edit_module(database_file, module_name, what, to, where):
-    db.edit_element(database_file, module_name, what, to, where)
+def remove_module(module_name):
+    db.remove_table('database/modules.db', module_name)
+
+def get_module(module_name):
+    return db.get_table_as_dict('database/modules.db', module_name, "id")
+
+def edit_module(module_name, what, to, where):
+    db.edit_element('database/modules.db', module_name, what, to, where)
+
+def get_tags(module_name):
+    return [tag for tag in db.get_table_as_dict('database/modules.db', module_name, "id")['tag_groups'].split()]
+
+#def add_tag(module_name, tag):
+#    db.edit_element('database/modules.db', module_name, 'tag_groups', ' '.join(get_tags(module_name).append(tag)), wh
+
+create_module('roupas')
